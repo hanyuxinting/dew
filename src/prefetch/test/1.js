3a7a5e0ad927e46506b4531bdc74a69c4b5484cd
lifesinger <lifesinger@gmail.com>
2011-10-17 20:37:34 +0800

    add package.json files

--- src/prefetch/test/1.js
+++
@@ -1,4 +0,0 @@
-
-
-document.getElementById('out1js')
-    .appendChild(document.createTextNode('1.js is loaded.'));
--- src/prefetch/test/2.js
+++
@@ -1,4 +0,0 @@
-
-
-document.getElementById('out2js')
-    .appendChild(document.createTextNode('2.js is loaded.'));
